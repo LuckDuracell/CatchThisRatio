892bfe9205bd2b00df337c8da9f7de348674377a
LuckDev <lukedru@yahoo.com>
2023-01-28 15:33:31 -0800

    Initial Commit

---
+++ CatchThisRatio/CatchThisRatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchThisRatioApp.swift
+//  CatchThisRatio
+//
+//  Created by Luke Drushell on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CatchThisRatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
